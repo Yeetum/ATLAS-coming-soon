9e497fecf5c7917c54ab8927c9ddf2e6cdd4e816
Saoming <Hujanesword@gmail.com>
2021-03-11 02:16:21 +0700

    update ui a bit

--- pages/index.vue
+++ pages/index.vue
@@ -5,11 +5,11 @@
       <div class="py-8 md:py-12 flex justify-center">
         <img src="~/assets/logo-2-white.png" class="z-50" />
       </div>
-      <div class="flex flex-col md:flex-row justify-between py-8 md:py-16 ">
+      <div class="flex flex-col md:flex-row justify-between md:py-16  mb-4 ">
         <!-- Left -->
         <div class="flex flex-col justify-center md:justify-start relative items-center md:items-stretch order-2 md:order-1">
           <h3 class="coming-soon-radius">Our website in under construction</h3>
-          <h1 class="text-4xl mb-4 font-light text-center md:text-left">Our platform is launching soon</h1>
+          <h1 class=" text-2xl md:text-4xl mb-4 font-light text-center md:text-left">Our platform is launching soon</h1>
           <p class="text-base coming-soon-paragraph mb-12 text-center md:text-left">ATLAS is a market intelligence (MARKINT) SaaS platform tool used for determining, predicting, and analyzing financial markets. </p>
           <Form />
         </div>
